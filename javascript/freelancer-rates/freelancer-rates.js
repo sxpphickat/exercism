2064646892064eacb46fa4a3e93592572eae6c22
sxpphickat <sphhhsh@gmail.com>
2023-10-24 13:48:45 -0300

    add: freelancer-rates

---
+++ javascript/freelancer-rates/freelancer-rates.js
@@ -0,0 +1,83 @@
+// @ts-check
+//
+// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion on the web
+// and supported IDEs when implementing this exercise. You don't need to
+// understand types, JSDoc, or TypeScript in order to complete this JavaScript
+// exercise, and can completely ignore this comment block and directive.
+
+// 👋🏽 Hi again!
+//
+// A quick reminder about exercise stubs:
+//
+// 💡 You're allowed to completely clear any stub before you get started. Often
+// we recommend using the stub, because they are already set-up correctly to
+// work with the tests, which you can find in ./freelancer-rates.spec.js.
+//
+// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
+// in idiomatic JavaScript, but some companies and style-guides do enforce them.
+//
+// Get those rates calculated!
+
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @returns {number} the rate per day
+ */
+export function dayRate(ratePerHour) {
+  return 8 * ratePerHour;
+}
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+export function daysInBudget(budget, ratePerHour) {
+  return Math.floor(budget / dayRate(ratePerHour));
+}
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ *
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+
+export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+  const totalDaysInMonth = 22;
+
+  // days with and without discount
+  const fullMonths    = Math.floor(numDays / totalDaysInMonth);
+  const remainingDays = numDays % totalDaysInMonth;
+  
+  // total without discount
+  const totalMonthsPrice = fullMonths * totalDaysInMonth * dayRate(ratePerHour);
+  const remainingDaysPrice = remainingDays * dayRate(ratePerHour);
+
+  //discount
+  const discountAmount = totalMonthsPrice * discount;
+  const priceWithDiscount = totalMonthsPrice - discountAmount;
+
+  //total
+  const totalPrice = Math.ceil(priceWithDiscount + remainingDaysPrice);
+
+  return totalPrice;
+
+/*
+ * raw unoptimized logic.
+
+    const daysWithDiscount = Math.floor(numDays / 22) * 22;
+  const daysWithoutDiscount = numDays - daysWithDiscount;
+  const totalMonths = daysWithDiscount * dayRate(ratePerHour);
+  const totalDays = daysWithoutDiscount * dayRate(ratePerHour);
+  const discountAmount = totalMonths * discount;
+  const priceWtihDiscount = totalMonths - discountAmount;
+  const total = Math.ceil(priceWtihDiscount + totalDays);
+  return total; */
+}
